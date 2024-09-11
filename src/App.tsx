641199f2e76f37458587e124de7afbf2e50e99a7
DevMHOne <110272440+DevMHOne@users.noreply.github.com>
2024-09-11 18:58:13 +0700

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
         setHolidays(data);
         setLoadingLibur(false);
       } catch (error) {
-        console.error("Terjadi kesalahan saat mengambil hari libur:", error);
+        console.error("Error fetching holidays:", error);
         setLoadingLibur(false);
       }
     }
@@ -64,11 +64,14 @@ function App() {
             target="_new"
             href="https://biolink.eu.org/fuds"
           >
-            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 6v12m8-12v12l-8-6 8-6Z"/>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-gift">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  <path d="M3 8m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z" />
+  <path d="M12 8l0 13" />
+  <path d="M19 12v7a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-7" />
+  <path d="M7.5 8a2.5 2.5 0 0 1 0 -5a4.8 8 0 0 1 4.5 5a4.8 8 0 0 1 4.5 -5a2.5 2.5 0 0 1 0 5" />
 </svg>
-
-            Airdrop Hunter
+            AirDrop Hunter
           </a>
         </div>
       </div>
